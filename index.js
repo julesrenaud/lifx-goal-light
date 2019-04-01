ff8bba671214555afc33803034b4f943947c43d4
Jules Renaud <renaud.jules@gmail.com>
2019-03-31 23:56:54 -0400

    Now using path module to call listen.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const path = require('path');
 const Server = require('./lib/server');
 const nodemon = require('nodemon');
 
-nodemon({ script: './lib/listen.js' }).on('restart', () => {
+nodemon({ script: path.resolve('lib/listen.js') }).on('restart', () => {
   console.log('Script restarted');
 }).on('crash', () => {
   console.log('Script crashed, restarting');
